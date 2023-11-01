bcdd2005d386cf6f4076d9083ac7b4115d15d0ea
bluepeng9 <bluepeng9@gmail.com>
2023-11-01 10:46:59 +0900

    Initial commit

---
+++ src/test/java/com/example/springfileuploadtutorial/SpringFileUploadTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfileuploadtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileUploadTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
