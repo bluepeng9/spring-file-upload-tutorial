9a4093384e9d0e18b4c2687f1e63eb64fda19cdd
bluepeng9 <bluepeng9@gmail.com>
2023-11-05 09:26:11 +0900

    feat: 파일 관련 예외 추가

--- src/main/java/com/example/springfileuploadtutorial/file/StorageException.java
+++ src/main/java/com/example/springfileuploadtutorial/file/StorageException.java
@@ -3,4 +3,8 @@ package com.example.springfileuploadtutorial.file;
 public class StorageException extends RuntimeException {
     public StorageException(String s) {
     }
+
+    public StorageException(String s, Throwable e) {
+
+    }
 }
